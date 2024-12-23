518606c269a08e3f20cba68fd19441dac5292ed0
sonuth4 <162896114+sonuth4@users.noreply.github.com>
2024-12-23 12:19:17 +0545

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,32 @@
+<?php
+
+$integerVar = 25;
+$floatVar = 10.5;
+$stringVar = "Hello, SONU!";
+$booleanVar = true;
+$arrayVar = array("apple", "banana", "cherry");
+
+echo "Using echo:<br>";
+echo "Integer: $integerVar<br>";
+echo "Float: $floatVar<br>";
+echo "String: $stringVar<br>";
+echo "Boolean: " . ($booleanVar ? "true" : "false") . "<br>";
+
+print "<br>Using print:<br>";
+print "Integer: $integerVar<br>";
+print "Float: $floatVar<br>";
+print "String: $stringVar<br>";
+print "Boolean: " . ($booleanVar ? "true" : "false") . "<br>";
+
+
+echo "<br>Array content using print_r:<br>";
+print_r($arrayVar);
+
+echo "<br><br>Array content using var_dump:<br>";
+var_dump($arrayVar);
+
+echo "<br><br>Data type checks:<br>";
+echo "Is \$integerVar an integer? " . (is_int($integerVar) ? "Yes" : "No") . "<br>";
+echo "Is \$floatVar a float? " . (is_float($floatVar) ? "Yes" : "No") . "<br>";
+echo "Is \$stringVar a string? " . (is_string($stringVar) ? "Yes" : "No") . "<br>";
+?>
